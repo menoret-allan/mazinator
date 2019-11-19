baf5055849f5c75a81b52a7d1ad4ab203bfb9444
Allan Menoret <allan.menoret@gmail.com>
2019-11-19 17:38:03 +0100

    optimize a bit the random again

--- Maze.Generator/RandomMazeGenerator.cs
+++ Maze.Generator/RandomMazeGenerator.cs
@@ -13,25 +13,25 @@ namespace MazeGenerator
             this.rand = new Random();
         }
 
-        private void Test(Maze maze, List<(int x, int y)> possibilities)
+        private void Test(Maze maze, HashSet<(int x, int y)> possibilities)
         {
             while (possibilities.Count > 0)
             {
-                var nextPath = this.rand.Next() % possibilities.Count;
-                var (x, y) = possibilities[nextPath];
+                var nextPath = this.rand.Next(0, possibilities.Count - 1);
+                var (x, y) = possibilities.ElementAt(nextPath);
 
                 maze.Board[y, x] = CaseType.Path;
                 possibilities.Remove((x, y));
-                foreach (var remove in GetPossibilities(x, y, maze))
+                foreach (var neighbour in GetPossibilities(x, y, maze))
                 {
-                    if (possibilities.Contains(remove))
+                    if (possibilities.Contains(neighbour))
                     {
-                        possibilities.Remove(remove);
-                        maze.Board[remove.y, remove.x] = CaseType.Wall;
+                        possibilities.Remove(neighbour);
+                        maze.Board[neighbour.y, neighbour.x] = CaseType.Wall;
                     }
                     else
                     {
-                        possibilities.Add(remove);
+                        possibilities.Add(neighbour);
                     }
                 }
             }
@@ -62,7 +62,7 @@ namespace MazeGenerator
             {
                 if (y > 1)
                 {
-                    if (maze[y-1, x] == CaseType.Unknow)
+                    if (maze[y - 1, x] == CaseType.Unknow)
                     {
                         yield return (x, y - 1);
                     }
@@ -84,7 +84,7 @@ namespace MazeGenerator
             maze.FillBoarderWith(CaseType.Wall);
             var entrance = (0, 1);
             this.OpenEntrance(maze, entrance);
-            this.Test(maze, new List<(int x, int y)>() { entrance });
+            this.Test(maze, new HashSet<(int x, int y)> { entrance });
             var exit = this.FindExit(maze);
             this.OpenExit(maze, exit);
 
@@ -107,7 +107,7 @@ namespace MazeGenerator
         {
             List<(int x, int y)> possibilities = new List<(int x, int y)>();
 
-            for (int rightSide = 1; rightSide < maze.Dimension.Y -1; rightSide++)
+            for (int rightSide = 1; rightSide < maze.Dimension.Y - 1; rightSide++)
             {
                 if (maze.Board[rightSide, maze.Dimension.X - 2] == CaseType.Path)
                 {
@@ -121,7 +121,7 @@ namespace MazeGenerator
                     possibilities.Add((downSide, maze.Dimension.Y - 1));
                 }
             }
-            
+
             return possibilities[this.rand.Next() % possibilities.Count];
         }
     }
